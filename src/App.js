248a99a399a7dfb75176b204aefea87321731c09
Angad Singh <mail@angad.dev>
2021-05-31 19:43:15 +0530

    feat: Write app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [repositories, setRepositories] = useState([
+    {
+      name: "adminer-serverless",
+      html_url: "https://github.com/dotangad/adminer-serverless",
+    },
+  ]);
+
+  const API_URL = (username) =>
+    `https://api.github.com/users/${username}/repos`;
+
+  const fetchData = async () => {
+    const data = await (await fetch(API_URL(username))).json();
+    setRepositories(
+      data.map((repo) => {
+        return {
+          name: repo.name,
+          html_url: repo.html_url,
+        };
+      })
+    );
+    setUsername("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub repository fetcher</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={fetchData}>Fetch</button>
+      </div>
+
+      <ul>
+        {repositories.map((repository) => (
+          <li>
+            <a target="_blank" href={repository.html_url}>
+              {repository.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
